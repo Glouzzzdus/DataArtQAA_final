868a800751cadaba7412595d1e8b9b229a4a38d2
Glouzzzdus <91818237+Glouzzzdus@users.noreply.github.com>
2023-05-23 08:02:54 +0200

    Uncomment report areas

--- Tests/BaseTest.cs
+++ Tests/BaseTest.cs
@@ -18,42 +18,42 @@ namespace DataArtQAA_Homework04
         protected ExtentReports extentReports;
         protected ExtentTest extentTest;
         protected PageList Pages { get; private set; }
-        //[OneTimeSetUp]
-        //protected void SetupReporting() 
-        //{
-        //    extentReports = new ExtentReports();
-        //    string reportPath = @"d:\reports\DA_report.html";
-        //    var htmlReporter = new ExtentHtmlReporter(reportPath);
-        //    extentReports.AttachReporter(htmlReporter);
-        //}
+        [OneTimeSetUp]
+        protected void SetupReporting()
+        {
+            extentReports = new ExtentReports();
+            string reportPath = @"d:\reports\DA_report.html";
+            var htmlReporter = new ExtentHtmlReporter(reportPath);
+            extentReports.AttachReporter(htmlReporter);
+        }
         [SetUp]
         public void Setup()
         {
             Driver = DriverHelper.GetDriver("chrome");
             Pages = new PageList(Driver);
-            //extentTest = extentReports.CreateTest(TestContext.CurrentContext.Test.FullName);
+            extentTest = extentReports.CreateTest(TestContext.CurrentContext.Test.FullName);
         }
 
         [TearDown]
         public void TearDown()
         {
-            //if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Passed)
-            //{
-            //    extentTest.Log(Status.Pass);
-            //}
-            //else
-            //{
-            //    var screenshotPath = DriverHelper.MakeScreenshot(Driver, testName: TestContext.CurrentContext.Test.MethodName);
-            //    extentTest.AddScreenCaptureFromPath(screenshotPath);
-            //    extentTest.Log(Status.Fail);
-            //}
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Passed)
+            {
+                extentTest.Log(Status.Pass);
+            }
+            else
+            {
+                var screenshotPath = DriverHelper.MakeScreenshot(Driver, testName: TestContext.CurrentContext.Test.MethodName);
+                extentTest.AddScreenCaptureFromPath(screenshotPath);
+                extentTest.Log(Status.Fail);
+            }
             Driver.Quit();
         }
 
-        //[OneTimeTearDown]
-        //public void ReportFlush()
-        //{
-        //    extentReports.Flush();
-        //}
+        [OneTimeTearDown]
+        public void ReportFlush()
+        {
+            extentReports.Flush();
+        }
     }
 }
